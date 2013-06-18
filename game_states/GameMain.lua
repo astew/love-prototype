8a3bf845c4177d027667d192740628e630adb77f
Brian Moore <moore.brian.d@gmail.com>
2013-06-18 12:26:36 -0400

    added map manager, adding animation library for lua

--- game_states/GameMain.lua
+++ game_states/GameMain.lua
@@ -1,9 +1,6 @@
 
 local GameState = require "class/GameState"
 
-local loader = require("lib/Advanced-Tiled-Loader/Loader")
-loader.path = "res/maps/"
-
 local HC = require("lib/HardonCollider")
 
 local hud = require("HealthHud")
@@ -12,6 +9,7 @@ local UpdateMgr = require("mgr/UpdateMgr")
 local KeyMgr = require("mgr/KeyMgr")
 local CollisionMgr = require("mgr/CollisionMgr")
 local ImgMgr = require("mgr/ImgMgr")
+local MapMgr = require("mgr/MapMgr")
 
 local collider
 local allSolidTiles
@@ -25,34 +23,34 @@ function GameMain:load(...)
 	self.done_callback = arg[1]
 
     print("Started")
-    map = loader.load("level.tmx")
+    MapMgr:loadMap("level2.tmx")
     collider = HC(64, on_collide, collision_stop)
     allSolidTiles = self:findSolidTiles(map)
-	
+
 	ImgMgr:loadImage("bullet", 	"res/graphics/bullet.png")
-	
+
     self:setupHero(32,32)
 	hud:init(Hero)
-	
+
 	self:setupIfaceMgr()
 	self:setupUpdateMgr()
 	self:setupKeyMgr()
 	self:setupCollisionMgr()
-	
+
 	self:create_bullet_factory()
 end
 
 function GameMain:setupHero(x, y)
 	Hero = require("Hero")
 	Hero:init(collider,x,y)
-	
+
 	Hero.onDeath_callback = function()	Hero:setPosition(32,32)	end
 	Hero.noLives_callback = self.done_callback
 end
 
 
 function GameMain:setupIfaceMgr()
-	IfaceMgr:addItem(map)
+	IfaceMgr:addItem(MapMgr.map)
 	IfaceMgr:addItem(collider)
 	IfaceMgr:addItem(Hero)
 	IfaceMgr:addItem(hud)
@@ -66,78 +64,72 @@ function GameMain:setupUpdateMgr()
 	love.focus = function(f)
 		if (not f) then	UpdateMgr:pause()	end
 	end
-	
-	
+
+
 	UpdateMgr:addItem(Hero)
 	UpdateMgr:addItem(collider)
-	
+
 	UpdateMgr:unpause()
 end
 
 function GameMain:setupKeyMgr()
 	love.keypressed = function(key, unicode)	KeyMgr:keyPressed(key)	end
 	love.keyreleased = function(key, unicode)	KeyMgr:keyReleased(key)	end
-	
+
 	local leftkeys = {left=0,a=0}
 	local rightkeys = {right=0,d=0}
-	
+
 	KeyMgr:setPressBinding({up=0,w=0}, function() Hero:jump() end)
 	KeyMgr:setPressBinding(" ", function() UpdateMgr:togglePause() end)
 	KeyMgr:setPressBinding(leftkeys, function() Hero:moveDir(-1) end)
 	KeyMgr:setPressBinding(rightkeys, function() Hero:moveDir(1) end)
-	
+
 	KeyMgr:setReleaseBinding(leftkeys, function() if (Hero:getMoveDir() == -1) then Hero:moveDir(0) end end)
 	KeyMgr:setReleaseBinding(rightkeys, function() if (Hero:getMoveDir() == 1) then Hero:moveDir(0) end end)
 end
 
 function GameMain:setupCollisionMgr()
 	CollisionMgr:init(collider)
-	
+
 	local coll = function(dt,a,b,dx,dy)	self:on_collide(dt,a,b,dx,dy) end
 	local uncoll = function(dt,a,b)	self:collision_stop(dt,a,b) end
-	
+
 	CollisionMgr:setCallbacks("hero", "tile", coll, uncoll)
 end
 
-function GameMain:findSolidTiles(map)
+function GameMain:findSolidTiles()
     local collidable_tiles = {}
-    local layer = map.layers["ground"]
-
-    for x, y, tile in map("ground"):iterate() do
-        if tile and tile.properties.solid then
-            local ctile = collider:addRectangle(x*16, y*16, 16, 16)
-         --   ctile.type = "tile"
-			ctile.coll_class = "tile"
-            collider:addToGroup("tiles", ctile)
-            collider:setPassive(ctile)
-            if tile.properties.hurty then
-                ctile.hurty = true
-            end
-            table.insert(collidable_tiles, ctile)
+    MapMgr:iterateLayerTilesByType("ground", "solid", function(x, y, tile)
+        local ctile = collider:addRectangle(x*16, y*16, 16, 16)
+        ctile.coll_class = "tile"
+        collider:addToGroup("tiles", ctile)
+        collider:setPassive(ctile)
+        if tile.properties.hurty then
+            ctile.hurty = true
         end
-    end
-
+        table.insert(collidable_tiles, ctile)
+    end)
     return collidable_tiles
 end
 
 function GameMain:add_bullet()
 	b_count = b_count + 1
 	local bullet = {}
-	
+
 	bullet.speed = 100
 	bullet.shape = collider:addPoint(32,200)
 	bullet.shape.coll_class = "bullet" .. b_count
-	
+
 	bullet.update = function(self, dt)
 		self.shape:move(dt*100,0)
 	end
-	
+
 	bullet.draw = function(self)
 		local x,y = self.shape:center()
 		ImgMgr:draw("bullet", x-16,y)
 	end
-	
-	
+
+
 	local on_collide = function(dt, shape_a, shape_b, dx, dy)
 		UpdateMgr:removeItem(bullet)
 		IfaceMgr:removeItem(bullet)
@@ -145,9 +137,9 @@ function GameMain:add_bullet()
 		CollisionMgr:setCallbacks(bullet.shape.coll_class, "tile", nil, nil)
 		CollisionMgr:setCallbacks(bullet.shape.coll_class, "hero", nil, nil)
 	end
-	
+
 	local hero_collide = function(dt, shape_a, shape_b, dx, dy)
-		
+
 		Hero:setYVelocity(-350)
 		Hero:damage(45)
 		UpdateMgr:removeItem(bullet)
@@ -156,8 +148,8 @@ function GameMain:add_bullet()
 		CollisionMgr:setCallbacks(bullet.shape.coll_class, "tile", nil, nil)
 		CollisionMgr:setCallbacks(bullet.shape.coll_class, "hero", nil, nil)
 	end
-	
-	
+
+
 	UpdateMgr:addItem(bullet)
 	IfaceMgr:addItem(bullet)
 	CollisionMgr:setCallbacks(bullet.shape.coll_class, "tile", on_collide)
@@ -170,13 +162,13 @@ function GameMain:create_bullet_factory()
 	local sself = self
 	factory.update = function(self, dt)
 		time_delta = time_delta + dt
-		
+
 		if(time_delta > 0.5) then
 			sself:add_bullet()
 			time_delta = 0
 		end
 	end
-	
+
 	UpdateMgr:addItem(factory)
 end
 
---
+++ mgr/MapMgr.lua
@@ -0,0 +1,25 @@
+
+
+local MapMgr = {}
+
+MapMgr.loader = require("lib/Advanced-Tiled-Loader/Loader")
+MapMgr.loader.path = "res/maps/"
+MapMgr.map = {}
+MapMgr.SolidTiles = {}
+
+	function MapMgr:loadMap(map_file)
+        for k,v in pairs(self.loader) do
+            print(k,v)
+        end
+        self.map = self.loader.load(map_file)
+	end
+
+    function MapMgr:iterateLayerTilesByType(layer, tileType, callback)
+        for x, y, tile in self.map(layer):iterate() do
+            if tile and tile.properties[tileType] then
+                callback(x, y, tile)
+            end
+        end
+    end
+
+return MapMgr
